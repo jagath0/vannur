d73bc12121a096be92a9a73ee6d733f6a89e706e
Jagath0 <jagathgowfa1997@gmai8l.com>
2022-12-21 09:29:04 +0530

    committt

---
+++ src/main/java/com/jen1/framework/J.java
@@ -0,0 +1,12 @@
+package com.jen1.framework;
+
+import org.testng.annotations.Test;
+
+public class J {
+
+	@Test
+	public void test1() {
+		System.out.println("j1 executing");
+		System.out.println("modified");
+}
+}

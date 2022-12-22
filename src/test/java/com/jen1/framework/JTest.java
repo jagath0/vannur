d5f71a553790b3cd190472b5ddbc59e94cd5edf5
jagath0 <120622197+jagath0@users.noreply.github.com>
2022-12-22 20:38:41 +0530

    up

--- src/test/java/com/jen1/framework/JTest.java
+++ src/test/java/com/jen1/framework/JTest.java
@@ -1,6 +1,8 @@
 package com.jen1.framework;
 import org.testng.annotations.Test;
 public class JTest {
+	//
+	//
 	@Test(groups = "sanity")
 	public void test1() {
 		System.out.println("j1 executing");
